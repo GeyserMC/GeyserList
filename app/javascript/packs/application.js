9421ac348b02f400b21c4f6d4b30773f3b5f8da1
Chew <chew@chew.pw>
2020-11-28 13:55:02 -0600

    Actually require the right things

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,9 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("@fortawesome/fontawesome-free/js/all")
-require("jquery")
-require("bootstrap")
-require("popper")
+require("jquery/dist/jquery")
+require("bootstrap/dist/js/bootstrap")
+require("@popperjs/core/dist/cjs/popper")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
