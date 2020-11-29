6a78c43d4e13e4a528f28e57e3b58869c39a8482
rtm516 <ryantmilner@hotmail.co.uk>
2020-11-29 16:19:53 +0000

    Add copy button to the IPs of servers (broken)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("@fortawesome/fontawesome-free/js/all")
 require("bootstrap/dist/js/bootstrap")
+require("clipboard")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
