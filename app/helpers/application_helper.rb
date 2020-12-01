ca7a4b167ed05da37ca83d7e801814e16e92d994
Chew <chew@chew.pw>
2020-11-30 20:09:13 -0600

    meta_tags helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  # Adds necessary tags for meta where needed
+  # @param title [String] The title of the page. Appears in navbar and embed titles. Do not include "- GeyserList"
+  # @param description [String] Embed description
+  # @param color [String] Embed color
+  def meta_tags(title: 'GeyserList', description: 'GeyserList', color: '#2b5797')
+    [
+      tag.title("#{title} - GeyserList"),
+      tag('meta', property: 'og:title', content: "#{title} - GeyserList"),
+      tag('meta', property: 'twitter:title', content: "#{title} - GeyserList"),
+      tag('meta', property: 'description', content: description.to_s),
+      tag('meta', property: 'og:description', content: description.to_s),
+      tag('meta', name: 'theme-color', content: color)
+    ].join("\n").html_safe
+  end
 end
