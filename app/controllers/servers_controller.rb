cbfd5a8aa236bdddc11b97a5f683889abdc3888e
Chew <chew@chew.pw>
2020-12-02 11:49:48 -0600

    Grab server list from database & render 404 if server not found

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -1,2 +1,15 @@
 class ServersController < ApplicationController
+  def index
+    @servers = Server.all.limit(100)
+  end
+
+  def show
+    @server = Server.find_by(id: params[:id])
+    
+    if @server.nil?
+      respond_to do |format|
+        format.html { render file: "#{Rails.root}/public/404.html", status: 404, :layout => false }
+      end
+    end
+  end
 end
