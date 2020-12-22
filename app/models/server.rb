46e129dd27abddb740c910e9eab8c5c3bcc50b23
Chew <chew@chew.pw>
2020-12-22 00:19:32 -0600

    [Fixes #15] Render markdown in server descriptions

--- app/models/server.rb
+++ app/models/server.rb
@@ -6,4 +6,11 @@ class Server < ApplicationRecord
   validates :name, uniqueness: { message: "A server with that name already exists!" }
   validates :description, length: { in: 100..2500 }
   validates :bedrock_ip, :java_ip, presence: true, uniqueness: true
+
+  def rendered_description
+    render = Redcarpet::Render::HTML.new(escape_html: true, no_images: true, no_styles: true, no_links: true, prettify: true)
+    markdown = Redcarpet::Markdown.new(render)
+
+    markdown.render(description)
+  end
 end
