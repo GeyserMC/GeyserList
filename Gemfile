3584fca259573e185b2690289a58dc1fe7cc7eca
Chew <chew@chew.pw>
2020-12-24 18:45:11 -0600

    Add gem and basic user flags

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem 'bootsnap', '>= 1.4.2', require: false
 # Parses and renders markdown
 gem 'redcarpet'
 
+# For bit flags
+gem 'flag_shih_tzu'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
 
   validates :username, :access_token, presence: true
   validates :username, uniqueness: { message: "An account with that name already exists!" }
+
+  has_flags 1 => :moderator, # 1
+            2 => :staff,     # 2
+            3 => :verified,  # 4
+            4 => :admin,     # 8
+            5 => :developer, # 16
+            :column => 'status'
 end
