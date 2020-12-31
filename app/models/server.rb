7548b0738f7ea00b533c265317249e251efb9e56
Chew <chew@chew.pw>
2020-12-30 23:38:26 -0600

    Dont re-verify IP presences

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,7 +1,7 @@
 class Server < ApplicationRecord
   belongs_to :user
 
-  validates :name, :description, :java_ip, :bedrock_ip, presence: true
+  validates :name, :description, presence: true
   validates :name, length: { in: 1..64 }
   validates :name, uniqueness: { message: "A server with that name already exists!" }
   validates :description, length: { in: 100..2500 }
