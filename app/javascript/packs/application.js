32256e72b3ff549f585b6ec135882ba07a1c4e07
Chew <chew@chew.pw>
2021-10-21 00:03:22 -0500

    Update to Rails 6.1 and Ruby 3.0

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,10 +4,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require('@rails/ujs').start()
-require('turbolinks').start()
-require('@rails/activestorage').start()
-require('channels')
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
 require('bootstrap/dist/js/bootstrap')
 require('@fortawesome/fontawesome-free/js/all')
 
